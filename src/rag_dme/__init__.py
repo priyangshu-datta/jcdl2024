ab906f83eed6fe34b6b08f9946d07cf4971ca5af
Priyangshu Datta <84124979+priyangshu-datta@users.noreply.github.com>
2024-07-30 15:38:02 +0530

    Update __init__.py

--- src/rag_dme/__init__.py
+++ src/rag_dme/__init__.py
@@ -1,7 +1,11 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 from .extract_datasets import extract_datasets
 from .goauth import GenerativeServiceClient
 from .vectorDB import ChromaPersist
 
-print("in module")
 
 __all__ = ["extract_datasets", "GenerativeServiceClient", "ChromaPersist"]
