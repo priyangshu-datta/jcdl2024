b2055c162fc58b4ae3b6617e058111b4764ab588
Priyangshu Datta <84124979+priyangshu-datta@users.noreply.github.com>
2024-07-31 00:22:46 +0530

    Update __init__.py

--- src/rag_dme/__init__.py
+++ src/rag_dme/__init__.py
@@ -1,3 +1,5 @@
+from functools import partial
+from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,5 +9,8 @@ from .extract_datasets import extract_datasets
 from .goauth import GenerativeServiceClient
 from .vectorDB import ChromaPersist
 
+chromaDB = ChromaPersist(name="embeds", path=Path(".local"))
+gsc = GenerativeServiceClient()
+extract_datasets = partial(extract_datasets, chromaDB=chromaDB, gsc=gsc)
 
 __all__ = ["extract_datasets", "GenerativeServiceClient", "ChromaPersist"]
