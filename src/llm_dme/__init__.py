6c0fa554373d675156114144459e141fee414133
Priyangshu Datta <84124979+priyangshu-datta@users.noreply.github.com>
2024-07-31 00:11:04 +0530

    updated extract_datasets export

--- src/llm_dme/__init__.py
+++ src/llm_dme/__init__.py
@@ -1,3 +1,4 @@
+from functools import partial
 from dotenv import load_dotenv
 
 
@@ -5,6 +6,8 @@ load_dotenv()
 
 from .extract_datasets import extract_datasets
 from .goauth import GenerativeServiceClient
+
+extract_datasets = partial(extract_datasets, gsc=GenerativeServiceClient())
 
 __all__ = [
     "extract_datasets",
